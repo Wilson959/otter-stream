c2b10d5be66432e7ebae5fb619ae8fa26fe919f3
Peter Uadiale <fxavy02@gmail.com>
2024-06-08 02:03:44 +0100

    refactor: add logic to disable channel card if user is already in channel route

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,58 +4,68 @@ import { CheckCircle } from "@mui/icons-material";
 
 import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = ({ channelDetail, marginTop }) => (
-  <Box
-    sx={{
-      boxShadow: "none",
-      borderRadius: "20px",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      width: { xs: "350px", md: "320px" },
-      height: "326px",
-      margin: "auto",
-      marginTop,
-    }}
-  >
-    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
-      <CardContent
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          textAlign: "center",
-          color: "#fff",
-        }}
-      >
-        <CardMedia
-          image={
-            channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
-          }
-          alt={channelDetail?.snippet?.title}
-          sx={{
-            borderRadius: "50%",
-            height: "180px",
-            width: "180px",
-            mb: 2,
-            border: "1px solid #e3e3e3",
-          }}
-        />
-        <Typography variant="h6">
-          {channelDetail?.snippet?.title}
-          <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
-        </Typography>
-        {channelDetail?.statistics?.subscriberCount && (
-          <Typography>
-            {parseInt(
-              channelDetail?.statistics?.subscriberCount
-            ).toLocaleString()}{" "}
-            Subscribers
-          </Typography>
-        )}
-      </CardContent>
-    </Link>
-  </Box>
-);
+const ChannelCard = ({ channelDetail, marginTop }) => {
+  const isInUrl = window.location.href.includes("channel");
+
+  return (
+    <Box
+      sx={{
+        boxShadow: "none",
+        borderRadius: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: { xs: "350px", md: "320px" },
+        height: "326px",
+        margin: "auto",
+        marginTop,
+      }}
+    >
+      {!isInUrl ? (
+        <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+          <Card channelDetail={channelDetail} />
+        </Link>
+      ) : (
+        <Card channelDetail={channelDetail} />
+      )}
+    </Box>
+  );
+};
 
 export default ChannelCard;
+
+const Card = ({ channelDetail }) => (
+  <CardContent
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      textAlign: "center",
+      color: "#fff",
+    }}
+  >
+    <CardMedia
+      image={
+        channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture
+      }
+      alt={channelDetail?.snippet?.title}
+      sx={{
+        borderRadius: "100px",
+        height: "180px",
+        width: "180px",
+        mb: 2,
+        border: "1px solid #e3e3e3",
+      }}
+    />
+    <Typography variant="h6">
+      {channelDetail?.snippet?.title}
+      <CheckCircle sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
+    </Typography>
+    {channelDetail?.statistics?.subscriberCount && (
+      <Typography>
+        {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()}{" "}
+        Subscribers
+      </Typography>
+    )}
+  </CardContent>
+);
