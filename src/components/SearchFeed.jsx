6bd3fcc8573dd010969ef6320ac1f1e49de002f9
Peter Uadiale <fxavy02@gmail.com>
2024-06-08 02:07:15 +0100

    refactor: modify search feed styles

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -19,7 +19,8 @@ const SearchFeed = () => {
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "#fff" }}>
-        Search Results for: <span style={{ color: "#f31503" }}>{searchTerm}</span>
+        Search Results for:{" "}
+        <span style={{ color: "var(--text-primary)" }}>{searchTerm}</span>
       </Typography>
 
       <Videos videos={videos} />
