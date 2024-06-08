65a3b10ab8511bd2952a6e3c597d5404d0a059da
Peter Uadiale <fxavy02@gmail.com>
2024-06-08 02:06:47 +0100

    refactor: modify search bar styles

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -25,23 +25,40 @@ const SearchBar = () => {
       onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
-        border: "1px solid #e3e3e3",
         pl: 2,
         boxShadow: "none",
+        width: "100%",
+        maxWidth: "480px",
+        height: "45px",
         mr: { sm: 5 },
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "var(--bg-primary)",
+        border: "2px solid transparent",
+
+        "&:focus-within": {
+          border: "2px solid var(--text-primary)",
+        },
       }}
     >
       <input
         className="search-bar"
         placeholder="Search..."
         value={searchTerm}
+        style={{
+          height: "40px",
+          width: { sm: "100%", md: "max-content" },
+          backgroundColor: "var(--bg-primary)",
+          color: "white",
+        }}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       <IconButton
         type="submit"
         sx={{
           p: "10px",
-          color: "red",
+          color: "var(--text-primary-dark)",
         }}
       >
         <Search />
