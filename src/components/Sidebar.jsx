7e0e40d42313680c638b8198b3ff26edb046b432
Peter Uadiale <fxavy02@gmail.com>
2024-06-08 02:07:54 +0100

    refactor: modify sidebar styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,75 +1,57 @@
 import { Stack } from "@mui/material";
-
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
-import HomeIcon from "@mui/icons-material/Home";
-import CodeIcon from "@mui/icons-material/Code";
-import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
-import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
-import LiveTvIcon from "@mui/icons-material/LiveTv";
-import SchoolIcon from "@mui/icons-material/School";
-import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
-import CheckroomIcon from "@mui/icons-material/Checkroom";
-import GraphicEqIcon from "@mui/icons-material/GraphicEq";
-import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
-import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
-import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
-
-const categories = [
-  { name: "New", icon: <HomeIcon /> },
-  { name: "JS Mastery", icon: <CodeIcon /> },
-  { name: "Coding", icon: <CodeIcon /> },
-  { name: "ReactJS", icon: <CodeIcon /> },
-  { name: "Strawberry Park", icon: <TheaterComedyIcon /> },
-  { name: "NextJS", icon: <CodeIcon /> },
-  { name: "Music", icon: <MusicNoteIcon /> },
-  { name: "Education", icon: <SchoolIcon /> },
-  { name: "Podcast", icon: <GraphicEqIcon /> },
-  { name: "Movie", icon: <OndemandVideoIcon /> },
-  { name: "Gaming", icon: <SportsEsportsIcon /> },
-  { name: "Live", icon: <LiveTvIcon /> },
-  { name: "Sport", icon: <FitnessCenterIcon /> },
-  { name: "Fashion", icon: <CheckroomIcon /> },
-  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
-  { name: "Comedy", icon: <TheaterComedyIcon /> },
-  { name: "Gym", icon: <FitnessCenterIcon /> },
-  { name: "Crypto", icon: <DeveloperModeIcon /> },
-];
+import { categories } from "../utils/constants";
 
 const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
+      flex: 1,
       overflowY: "auto",
       height: { sx: "auto", md: "95%" },
       flexDirection: { md: "column" },
+      width: "250px",
+      backgroundColor: "var(--bg-secondary)",
+      p: { sx: 0, md: 2 },
     }}
   >
-    {categories.map((category, index) => (
-      <button
-        key={Math.random() + index}
-        className="category-btn"
-        onClick={() => setSelectedCategory(category.name)}
-        style={{
-          background: category.name === selectedCategory && "hsl(0, 100%, 32%)",
-          color: "#fff",
-        }}
-      >
-        <span
+    {categories.map((category, index) => {
+      const Icon = category.icon;
+
+      return (
+        <button
+          key={Math.random() + index}
+          className="category-btn"
+          onClick={() => setSelectedCategory(category.name)}
           style={{
-            color:
-              category.name === selectedCategory ? "#fff" : "hsl(0, 100%, 42%)",
-            marginRight: "15px",
+            background:
+              category.name === selectedCategory && "var(--text-primary-dark)",
+            color: "#fff",
           }}
         >
-          {category.icon}
-        </span>
-        <span
-          style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}
-        >
-          {category.name}
-        </span>
-      </button>
-    ))}
+          <span
+            style={{
+              color:
+                category.name === selectedCategory
+                  ? "#fff"
+                  : "var(--text-primary-dark)",
+              marginRight: "15px",
+            }}
+          >
+            <Icon />
+          </span>
+          <span
+            className={
+              category.name !== selectedCategory ? "category-btn-hover" : ""
+            }
+            style={{
+              opacity: category.name === selectedCategory ? "1" : "0.8",
+            }}
+          >
+            {category.name}
+          </span>
+        </button>
+      );
+    })}
   </Stack>
 );
 
