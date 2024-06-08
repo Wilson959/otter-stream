8f9d5bb8f409733111b01135dcd9370d2c06aed2
Peter Uadiale <fxavy02@gmail.com>
2024-06-08 02:00:39 +0100

    style: modify layout height

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import {
 
 const App = () => (
   <BrowserRouter>
-    <Box sx={{ backgroundColor: "#303030" }}>
+    <Box sx={{ backgroundColor: "var(--bg-primary)", minHeight: "100vh" }}>
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Feed />} />
