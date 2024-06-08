541b43eb50874417c374fb0d3eab33b5d507a16f
Peter Uadiale <fxavy02@gmail.com>
2024-06-08 02:05:59 +0100

    refactor: make navbar mobile responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,40 +1,30 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import { SearchBar } from "./";
 
-import { logo } from "../utils/constants";
+import Logo from "../assets/logo-light.svg";
 
 const Navbar = () => (
   <Stack
     direction="row"
     alignItems="center"
-    flexWrap="wrap"
-    p={2}
+    flexWrap={{ xs: "wrap", sm: "nowrap" }}
+    gap={1}
+    px={4}
+    py={1}
+    minHeight="80px"
     sx={{
       position: "sticky",
-      backgroundColor: "#2A272A",
+      backgroundColor: "var(--bg-secondary)",
       top: 0,
-      justifyContent: "space-between",
+      justifyContent: { xs: "center", sm: "space-between" },
       zIndex: "100",
+      borderBottom: "1px solid black",
+      alignItems: "center",
     }}
   >
-    <Link
-      to="/"
-      style={{ display: "flex", alignItems: "center", marginBottom: "15px" }}
-    >
-      <img src={logo} alt="riderx media logo" height={45} />
-      <Typography
-        component="p"
-        variant="h6"
-        sx={{
-          color: "#fff",
-          fontWeight: "bold",
-          ml: 1,
-          opacity: 0.8,
-        }}
-      >
-        YouTube
-      </Typography>
+    <Link to="/">
+      <img src={Logo} alt="riderx media logo" />
     </Link>
     <SearchBar />
   </Stack>
