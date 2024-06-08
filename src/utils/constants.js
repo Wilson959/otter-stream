e0724995444be3c381c8d40c21d2126ad7ab09a8
Peter Uadiale <fxavy02@gmail.com>
2024-06-08 02:10:10 +0100

    refactor: export categories constant data for sidebar

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,38 @@
-export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import AnimationOutlinedIcon from "@mui/icons-material/AnimationOutlined";
+
+export const categories = [
+  { name: "Latest", icon: HomeIcon },
+  { name: "Programming", icon: CodeIcon },
+  { name: "Anime", icon: AnimationOutlinedIcon },
+  { name: "Strawberry Park", icon: TheaterComedyIcon },
+  { name: "ReactJS", icon: CodeIcon },
+  { name: "Music", icon: MusicNoteIcon },
+  { name: "NextJS", icon: CodeIcon },
+  { name: "Education", icon: SchoolIcon },
+  { name: "Podcast", icon: GraphicEqIcon },
+  { name: "Movie", icon: OndemandVideoIcon },
+  { name: "Gaming", icon: SportsEsportsIcon },
+  { name: "Live", icon: LiveTvIcon },
+  { name: "Sport", icon: FitnessCenterIcon },
+  { name: "Fashion", icon: CheckroomIcon },
+  { name: "Beauty", icon: FaceRetouchingNaturalIcon },
+  { name: "Comedy", icon: TheaterComedyIcon },
+  { name: "Gym", icon: FitnessCenterIcon },
+  { name: "Crypto", icon: DeveloperModeIcon },
+];
 
 export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
 export const demoChannelUrl = "/channel/UCmXmlB4-HJytD7wek0Uo97A";
